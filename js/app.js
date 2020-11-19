0eda509e12858cb958d44aff8f5776cf70f9aa75
Oliver Payne <olliepayne@me.com>
2020-11-19 15:45:19 -0700

    add working cell event logic

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@ const board = {
     for(let i = 0; i < this.cellsX * this.cellsY; i++) {
       this.grid.push('');
 
+      // html side
       const newCellEl = document.createElement('div');
+      newCellEl.className = 'cell';
       newCellEl.id = `cell-${i}`;
       newCellEl.addEventListener('click', (e) => this.clickCell(e.target));
       this.gameBoardEl.appendChild(newCellEl);
@@ -17,6 +19,17 @@ const board = {
   },
   clickCell: function(clickedCell) {
     let i = this.cellEls.indexOf(clickedCell);
+    let colorToFill = '';
+    if(game.turn === 1) { 
+      colorToFill = player1.color;
+    } else if(game.turn === 2) {
+      colorToFill = player2.color;
+    }
+
+    if(i >= (this.cellsX * this.cellsY - 1) - (this.cellsX + 1) && i < this.cellsX * this.cellsY || this.grid[i + 7] === colorToFill) {
+      this.grid[i] = colorToFill; 
+      this.cellEls[i].style.backgroundColor = colorToFill;
+    }
   },
 }
 
@@ -31,10 +44,13 @@ class Player {
   }
 }
 const player1 = new Player();
+player1.color = 'yellow';
 const player2 = new Player();
+player2.color = 'red';
 
 const game = {
-  
+  turn: 0,
 }
 
 board.drawGrid();
+game.turn = 1;
